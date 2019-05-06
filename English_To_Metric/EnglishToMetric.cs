a982fa205681fe429147a8ce384cf854982e2f8e
antoinealex1234@gmail.com <doras9070>
2019-05-06 12:16:34 -0400

    Create a class called EnglishToMetric to preform the conversion from english to metric.

---
+++ English_To_Metric/EnglishToMetric.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace English_To_Metric
+{
+    class EnglishToMetric
+    {
+        private double centimeters;
+        private int meters;
+        private int kilometer;
+
+        public EnglishToMetric(string userMiles, string userYards, string userFeet, string userInches)
+        {
+
+            centimeters = milesToYards(int.Parse(userMiles)) + Convert.ToInt16(userFeet);
+        }
+
+        public int milesToYards(int miles)
+        {
+            return miles * 1760;
+        }
+
+        public int yardsToFeet(int yards)
+        {
+            return yards * 3;
+        }
+
+        public double feetToInches(int feet)
+        {
+            return feet * 2.54;
+        }
+
+
+    }
+}
