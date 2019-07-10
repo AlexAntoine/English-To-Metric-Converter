4ed9316b938250570efa06a6b57a640be7048e0e
antoinealex1234@gmail.com <doras9070>
2019-07-10 08:44:40 -0400

    created Rounding class to round the results two decimal places

---
+++ English_To_Metric/Rounding.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace English_To_Metric
+{
+    class Rounding
+    {
+        public Rounding()
+        {
+
+        }
+
+        public double round(double numerToRound, int numberPlace)
+        {
+            double tempDouble = (numerToRound * Math.Pow(10, numberPlace));
+
+            tempDouble += .5;
+
+            int tempInt = (int)tempDouble;
+
+            tempDouble = tempInt / Math.Pow(10, numberPlace);
+
+            return tempDouble;
+        }
+    }
+}
