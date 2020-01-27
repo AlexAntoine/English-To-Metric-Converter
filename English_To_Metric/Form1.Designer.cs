e69aef0d7d6a101bb33f8548c15f508a992e9169
Alex Antoine <antoinealex1234@gmail.com>
2020-01-27 04:36:37 -0500

    Changed Form layout

--- English_To_Metric/Form1.Designer.cs
+++ English_To_Metric/Form1.Designer.cs
@@ -78,11 +78,12 @@
             this.grpEnglishToMetric.Controls.Add(this.txtYards);
             this.grpEnglishToMetric.Controls.Add(this.txtFeet);
             this.grpEnglishToMetric.Controls.Add(this.txtMiles);
+            this.grpEnglishToMetric.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.grpEnglishToMetric.Location = new System.Drawing.Point(25, 140);
-            this.grpEnglishToMetric.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.grpEnglishToMetric.Margin = new System.Windows.Forms.Padding(2);
             this.grpEnglishToMetric.Name = "grpEnglishToMetric";
-            this.grpEnglishToMetric.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.grpEnglishToMetric.Size = new System.Drawing.Size(356, 136);
+            this.grpEnglishToMetric.Padding = new System.Windows.Forms.Padding(2);
+            this.grpEnglishToMetric.Size = new System.Drawing.Size(439, 246);
             this.grpEnglishToMetric.TabIndex = 1;
             this.grpEnglishToMetric.TabStop = false;
             this.grpEnglishToMetric.Text = "English";
@@ -92,49 +93,50 @@
             // lblInches
             // 
             this.lblInches.AutoSize = true;
-            this.lblInches.Location = new System.Drawing.Point(170, 93);
+            this.lblInches.Location = new System.Drawing.Point(295, 131);
             this.lblInches.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblInches.Name = "lblInches";
-            this.lblInches.Size = new System.Drawing.Size(39, 13);
+            this.lblInches.Size = new System.Drawing.Size(66, 24);
             this.lblInches.TabIndex = 8;
             this.lblInches.Text = "Inches";
             // 
             // lblEFeet
             // 
             this.lblEFeet.AutoSize = true;
-            this.lblEFeet.Location = new System.Drawing.Point(21, 92);
+            this.lblEFeet.Location = new System.Drawing.Point(42, 145);
             this.lblEFeet.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblEFeet.Name = "lblEFeet";
-            this.lblEFeet.Size = new System.Drawing.Size(28, 13);
+            this.lblEFeet.Size = new System.Drawing.Size(48, 24);
             this.lblEFeet.TabIndex = 7;
             this.lblEFeet.Text = "Feet";
             // 
             // lblYards
             // 
             this.lblYards.AutoSize = true;
-            this.lblYards.Location = new System.Drawing.Point(170, 30);
+            this.lblYards.Location = new System.Drawing.Point(303, 35);
             this.lblYards.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblYards.Name = "lblYards";
-            this.lblYards.Size = new System.Drawing.Size(34, 13);
+            this.lblYards.Size = new System.Drawing.Size(58, 24);
             this.lblYards.TabIndex = 6;
             this.lblYards.Text = "Yards";
             // 
             // lblEMiles
             // 
             this.lblEMiles.AutoSize = true;
-            this.lblEMiles.Location = new System.Drawing.Point(18, 30);
+            this.lblEMiles.Location = new System.Drawing.Point(42, 43);
             this.lblEMiles.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblEMiles.Name = "lblEMiles";
-            this.lblEMiles.Size = new System.Drawing.Size(31, 13);
+            this.lblEMiles.Size = new System.Drawing.Size(54, 24);
             this.lblEMiles.TabIndex = 5;
             this.lblEMiles.Text = "Miles";
             // 
             // btnEnglishClear
             // 
-            this.btnEnglishClear.Location = new System.Drawing.Point(300, 110);
-            this.btnEnglishClear.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnEnglishClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEnglishClear.Location = new System.Drawing.Point(332, 204);
+            this.btnEnglishClear.Margin = new System.Windows.Forms.Padding(2);
             this.btnEnglishClear.Name = "btnEnglishClear";
-            this.btnEnglishClear.Size = new System.Drawing.Size(52, 22);
+            this.btnEnglishClear.Size = new System.Drawing.Size(78, 38);
             this.btnEnglishClear.TabIndex = 4;
             this.btnEnglishClear.Text = "Clear";
             this.btnEnglishClear.UseVisualStyleBackColor = true;
@@ -142,35 +144,37 @@
             // 
             // txtInches
             // 
-            this.txtInches.Location = new System.Drawing.Point(215, 89);
-            this.txtInches.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtInches.Location = new System.Drawing.Point(281, 157);
+            this.txtInches.Margin = new System.Windows.Forms.Padding(2);
+            this.txtInches.Multiline = true;
             this.txtInches.Name = "txtInches";
-            this.txtInches.Size = new System.Drawing.Size(68, 20);
+            this.txtInches.Size = new System.Drawing.Size(98, 29);
             this.txtInches.TabIndex = 3;
             this.txtInches.TextChanged += new System.EventHandler(this.txtInches_TextChanged);
             // 
             // txtYards
             // 
-            this.txtYards.Location = new System.Drawing.Point(215, 26);
-            this.txtYards.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtYards.Location = new System.Drawing.Point(265, 71);
+            this.txtYards.Margin = new System.Windows.Forms.Padding(2);
+            this.txtYards.Multiline = true;
             this.txtYards.Name = "txtYards";
-            this.txtYards.Size = new System.Drawing.Size(68, 20);
+            this.txtYards.Size = new System.Drawing.Size(127, 30);
             this.txtYards.TabIndex = 2;
             // 
             // txtFeet
             // 
-            this.txtFeet.Location = new System.Drawing.Point(62, 89);
-            this.txtFeet.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtFeet.Location = new System.Drawing.Point(15, 171);
+            this.txtFeet.Margin = new System.Windows.Forms.Padding(2);
             this.txtFeet.Name = "txtFeet";
-            this.txtFeet.Size = new System.Drawing.Size(68, 20);
+            this.txtFeet.Size = new System.Drawing.Size(127, 29);
             this.txtFeet.TabIndex = 1;
             // 
             // txtMiles
             // 
-            this.txtMiles.Location = new System.Drawing.Point(62, 30);
-            this.txtMiles.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtMiles.Location = new System.Drawing.Point(15, 71);
+            this.txtMiles.Margin = new System.Windows.Forms.Padding(2);
             this.txtMiles.Name = "txtMiles";
-            this.txtMiles.Size = new System.Drawing.Size(68, 20);
+            this.txtMiles.Size = new System.Drawing.Size(127, 29);
             this.txtMiles.TabIndex = 0;
             // 
             // grpMetricToEnglish
@@ -182,11 +186,12 @@
             this.grpMetricToEnglish.Controls.Add(this.txtCentimeters);
             this.grpMetricToEnglish.Controls.Add(this.txtKilometers);
             this.grpMetricToEnglish.Controls.Add(this.txtMeters);
-            this.grpMetricToEnglish.Location = new System.Drawing.Point(422, 140);
-            this.grpMetricToEnglish.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.grpMetricToEnglish.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grpMetricToEnglish.Location = new System.Drawing.Point(497, 140);
+            this.grpMetricToEnglish.Margin = new System.Windows.Forms.Padding(2);
             this.grpMetricToEnglish.Name = "grpMetricToEnglish";
-            this.grpMetricToEnglish.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.grpMetricToEnglish.Size = new System.Drawing.Size(324, 136);
+            this.grpMetricToEnglish.Padding = new System.Windows.Forms.Padding(2);
+            this.grpMetricToEnglish.Size = new System.Drawing.Size(439, 246);
             this.grpMetricToEnglish.TabIndex = 2;
             this.grpMetricToEnglish.TabStop = false;
             this.grpMetricToEnglish.Text = "Metric";
@@ -195,39 +200,42 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(67, 90);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(118, 134);
             this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.Size = new System.Drawing.Size(123, 25);
             this.label2.TabIndex = 6;
             this.label2.Text = "centimeters";
             // 
             // lblKilometers
             // 
             this.lblKilometers.AutoSize = true;
-            this.lblKilometers.Location = new System.Drawing.Point(174, 41);
+            this.lblKilometers.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblKilometers.Location = new System.Drawing.Point(259, 30);
             this.lblKilometers.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblKilometers.Name = "lblKilometers";
-            this.lblKilometers.Size = new System.Drawing.Size(55, 13);
+            this.lblKilometers.Size = new System.Drawing.Size(113, 25);
             this.lblKilometers.TabIndex = 5;
             this.lblKilometers.Text = "Kilometers";
             // 
             // lblMeters
             // 
             this.lblMeters.AutoSize = true;
-            this.lblMeters.Location = new System.Drawing.Point(19, 41);
+            this.lblMeters.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMeters.Location = new System.Drawing.Point(48, 34);
             this.lblMeters.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblMeters.Name = "lblMeters";
-            this.lblMeters.Size = new System.Drawing.Size(39, 13);
+            this.lblMeters.Size = new System.Drawing.Size(78, 25);
             this.lblMeters.TabIndex = 4;
             this.lblMeters.Text = "Meters";
             // 
             // btnMetricClear
             // 
-            this.btnMetricClear.Location = new System.Drawing.Point(255, 110);
-            this.btnMetricClear.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnMetricClear.Location = new System.Drawing.Point(346, 204);
+            this.btnMetricClear.Margin = new System.Windows.Forms.Padding(2);
             this.btnMetricClear.Name = "btnMetricClear";
-            this.btnMetricClear.Size = new System.Drawing.Size(52, 22);
+            this.btnMetricClear.Size = new System.Drawing.Size(78, 38);
             this.btnMetricClear.TabIndex = 3;
             this.btnMetricClear.Text = "Clear";
             this.btnMetricClear.UseVisualStyleBackColor = true;
@@ -235,26 +243,29 @@
             // 
             // txtCentimeters
             // 
-            this.txtCentimeters.Location = new System.Drawing.Point(141, 86);
-            this.txtCentimeters.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtCentimeters.Location = new System.Drawing.Point(111, 171);
+            this.txtCentimeters.Margin = new System.Windows.Forms.Padding(2);
+            this.txtCentimeters.Multiline = true;
             this.txtCentimeters.Name = "txtCentimeters";
-            this.txtCentimeters.Size = new System.Drawing.Size(68, 20);
+            this.txtCentimeters.Size = new System.Drawing.Size(140, 30);
             this.txtCentimeters.TabIndex = 2;
             // 
             // txtKilometers
             // 
-            this.txtKilometers.Location = new System.Drawing.Point(240, 39);
-            this.txtKilometers.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtKilometers.Location = new System.Drawing.Point(234, 72);
+            this.txtKilometers.Margin = new System.Windows.Forms.Padding(2);
+            this.txtKilometers.Multiline = true;
             this.txtKilometers.Name = "txtKilometers";
-            this.txtKilometers.Size = new System.Drawing.Size(68, 20);
+            this.txtKilometers.Size = new System.Drawing.Size(138, 33);
             this.txtKilometers.TabIndex = 1;
             // 
             // txtMeters
             // 
-            this.txtMeters.Location = new System.Drawing.Point(62, 41);
-            this.txtMeters.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtMeters.Location = new System.Drawing.Point(23, 79);
+            this.txtMeters.Margin = new System.Windows.Forms.Padding(2);
+            this.txtMeters.Multiline = true;
             this.txtMeters.Name = "txtMeters";
-            this.txtMeters.Size = new System.Drawing.Size(68, 20);
+            this.txtMeters.Size = new System.Drawing.Size(127, 30);
             this.txtMeters.TabIndex = 0;
             // 
             // optEnglishToMetric
@@ -262,7 +273,7 @@
             this.optEnglishToMetric.AutoSize = true;
             this.optEnglishToMetric.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.optEnglishToMetric.Location = new System.Drawing.Point(148, 102);
-            this.optEnglishToMetric.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.optEnglishToMetric.Margin = new System.Windows.Forms.Padding(2);
             this.optEnglishToMetric.Name = "optEnglishToMetric";
             this.optEnglishToMetric.Size = new System.Drawing.Size(148, 24);
             this.optEnglishToMetric.TabIndex = 3;
@@ -274,8 +285,8 @@
             // 
             this.optMetricToEnglish.AutoSize = true;
             this.optMetricToEnglish.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.optMetricToEnglish.Location = new System.Drawing.Point(532, 101);
-            this.optMetricToEnglish.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.optMetricToEnglish.Location = new System.Drawing.Point(640, 92);
+            this.optMetricToEnglish.Margin = new System.Windows.Forms.Padding(2);
             this.optMetricToEnglish.Name = "optMetricToEnglish";
             this.optMetricToEnglish.Size = new System.Drawing.Size(148, 24);
             this.optMetricToEnglish.TabIndex = 4;
@@ -285,10 +296,11 @@
             // 
             // btnConvert
             // 
-            this.btnConvert.Location = new System.Drawing.Point(662, 328);
-            this.btnConvert.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnConvert.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConvert.Location = new System.Drawing.Point(427, 435);
+            this.btnConvert.Margin = new System.Windows.Forms.Padding(2);
             this.btnConvert.Name = "btnConvert";
-            this.btnConvert.Size = new System.Drawing.Size(73, 28);
+            this.btnConvert.Size = new System.Drawing.Size(101, 36);
             this.btnConvert.TabIndex = 5;
             this.btnConvert.Text = "Convert";
             this.btnConvert.UseVisualStyleBackColor = true;
@@ -296,10 +308,11 @@
             // 
             // btnExit
             // 
-            this.btnExit.Location = new System.Drawing.Point(49, 328);
-            this.btnExit.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(71, 435);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(2);
             this.btnExit.Name = "btnExit";
-            this.btnExit.Size = new System.Drawing.Size(73, 28);
+            this.btnExit.Size = new System.Drawing.Size(84, 36);
             this.btnExit.TabIndex = 6;
             this.btnExit.Text = "Exit";
             this.btnExit.UseVisualStyleBackColor = true;
@@ -307,9 +320,10 @@
             // 
             // btnReset
             // 
-            this.btnReset.Location = new System.Drawing.Point(325, 328);
+            this.btnReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.Location = new System.Drawing.Point(814, 435);
             this.btnReset.Name = "btnReset";
-            this.btnReset.Size = new System.Drawing.Size(73, 28);
+            this.btnReset.Size = new System.Drawing.Size(84, 36);
             this.btnReset.TabIndex = 7;
             this.btnReset.Text = "Reset";
             this.btnReset.UseVisualStyleBackColor = true;
@@ -319,7 +333,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(783, 376);
+            this.ClientSize = new System.Drawing.Size(956, 509);
             this.Controls.Add(this.btnReset);
             this.Controls.Add(this.btnExit);
             this.Controls.Add(this.btnConvert);
@@ -328,7 +342,7 @@
             this.Controls.Add(this.grpMetricToEnglish);
             this.Controls.Add(this.grpEnglishToMetric);
             this.Controls.Add(this.lblTitle);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "frmEnglishToMetric";
             this.Text = "English To Metric Converter";
             this.Load += new System.EventHandler(this.frmEnglishToMetric_Load);
